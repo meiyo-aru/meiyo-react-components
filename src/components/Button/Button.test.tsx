492ce70d04d802afb63981be57b29766af443966
blink992 <alleybossmangas@gmail.com>
2025-07-27 17:18:42 -0300

    Create button component and fix config archives

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { Button } from './Button'; 
+
+describe('Button', () => {
+  it('should render the button with the correct text', () => {
+    render(<Button text="Comprar Agora" />);
+    expect(screen.getByRole('button', { name: 'Comprar Agora' })).toBeInTheDocument();
+  });
+
+  it('should call events handler', () => {
+    const handleClick = vi.fn();
+    const handleHover = vi.fn();
+
+    render(<Button text="Adicionar ao Carrinho" onClick={handleClick} onMouseEnter={handleHover} onMouseLeave={handleHover} />);
+    const button = screen.getByRole('button', { name: 'Adicionar ao Carrinho' });
+
+    fireEvent.click(button);
+    expect(handleClick).toHaveBeenCalledTimes(1);
+    fireEvent.mouseEnter(button); 
+    expect(handleHover).toHaveBeenCalledTimes(1);
+
+    fireEvent.mouseLeave(button); 
+    expect(handleHover).toHaveBeenCalledTimes(1);
+  });
+
+
+
+  it('should not call events when disabled', () => {
+    const handleClick = vi.fn();
+    const handleHover = vi.fn();
+
+    render(<Button text="Desabilitado" onClick={handleClick} onMouseEnter={handleHover} onMouseLeave={handleHover} disabled />);
+    const button = screen.getByRole('button', { name: 'Desabilitado' });
+
+    fireEvent.click(button);
+    expect(handleClick).not.toHaveBeenCalled();
+
+    fireEvent.mouseEnter(button)
+    expect(handleHover).not.toHaveBeenCalled();
+    
+    fireEvent.mouseLeave(button)
+    expect(handleHover).not.toHaveBeenCalled();
+  });
+});
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,52 @@
+import type React from "react";
+import style from "./Button.module.scss"
+
 interface ButtonProps {
-    text: string
+    text: string,
+    onClick?: React.MouseEventHandler,
+    onMouseEnter?: React.MouseEventHandler,
+    onMouseLeave?: React.MouseEventHandler,
+    disabled?: boolean,
+    type?: "primary" | "secondary" | "success" | "danger" | "warning",
+    boxShadow?: boolean,
+    hover?: boolean,
 }
 
 export const Button: React.FC<ButtonProps> = ({
-    text
+    ...props
 }) => {
+
+    let type = style.primary;
+    switch (props.type) {
+        case "secondary":
+            type = style.secondary;
+            break;
+        case "success":
+            type = style.success;
+            break;
+        case "danger":
+            type = style.danger;
+            break;
+        case "warning":
+            type = style.warning;    
+            break;
+        default:
+            break;
+    }
+
     return (
-        <button>{text}</button>
+        
+            props.disabled ?
+                    <button disabled={props.disabled}  className={`${style.disabled}`}>Disabled</button>
+                :
+                    <button 
+                    className={`${type} ${props.boxShadow && style.shadow} ${props.hover && style.hover}`} 
+                    onClick={props.onClick} 
+                    onMouseEnter={props.onMouseEnter} 
+                    onMouseLeave={props.onMouseLeave}
+                    disabled={props.disabled}>
+                        {props.text}
+                    </button>
+        
     );
 };
